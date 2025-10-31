82d04a907fe11429db31f6c52a3a82545633efe8
ShreyPatel383 <patelshrey16@gmail.com>
2025-10-31 19:43:06 +0000

    Initial discord OAuth verification.

---
+++ PokemonCardGameBot/Commands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Discord;
+
+public class Commands
+{
+	public Commands()
+	{
+
+	}
+	public async Task Client_Ready()
+	{
+        var guild = client.GetGuild(guildId);
+        var guildCommand = new SlashCommandBuilder();
+        guildCommand.WithName("test1");
+        guildCommand.WithDescription("TESTING COMPLETE HELLO BITCH");
+    }
+}
--- PokemonCardGameBot/Program.cs
+++ PokemonCardGameBot/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    private static DiscordSocketClient _client;
+    public static async Task Main()
+    {
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+
+        var token = File.ReadAllText("F:\\DiscordToken\\token.txt");
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        await Task.Delay(-1);
+
+    }
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+
+}
