c37ef7c4f6d446c757fb832dc9974b21022e10bc
ShreyPatel383 <patelshrey16@gmail.com>
2025-11-01 02:30:55 +0000

    Created discord client and event manager to allow bot to join channels.

--- PokemonCardGameBot/Commands.cs
+++ PokemonCardGameBot/Commands.cs
@@ -1,17 +1,42 @@
-﻿using System;
-using Discord;
+﻿using Discord;
+using Discord.Net;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
 
 public class Commands
 {
-	public Commands()
+    
+    private readonly DiscordSocketClient client;
+    public Commands(DiscordSocketClient _client)
 	{
+        client = _client;
+        client.Ready += Client_Ready;
+        client.SlashCommandExecuted += SlashCommandHandler;
+    }
 
-	}
 	public async Task Client_Ready()
 	{
-        var guild = client.GetGuild(guildId);
+
+        var guild = client.GetGuild(712014454098755584);
         var guildCommand = new SlashCommandBuilder();
         guildCommand.WithName("test1");
-        guildCommand.WithDescription("TESTING COMPLETE HELLO BITCH");
+        guildCommand.WithDescription("first test command");
+        
+
+        try {
+            await guild.CreateApplicationCommandAsync(guildCommand.Build());
+        }
+        catch (ApplicationCommandException exception)
+        {
+            var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+
+    private async Task SlashCommandHandler(SocketSlashCommand command)
+    {
+        await command.RespondAsync($"TESTING HELLO HELLO HELLO BITCH");
     }
 }
--- PokemonCardGameBot/Program.cs
+++ PokemonCardGameBot/Program.cs
@@ -1,26 +1,44 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using System.Diagnostics;
 
 public class Program
 {
-    private static DiscordSocketClient _client;
-    public static async Task Main()
+    public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+    private DiscordSocketClient _client;
+    private Commands commands;
+
+   // public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+    public async Task MainAsync()
     {
-        _client = new DiscordSocketClient();
+        var config = new DiscordSocketConfig()
+        {
+            GatewayIntents = GatewayIntents.Guilds |
+                             GatewayIntents.GuildMessages |
+                             GatewayIntents.MessageContent
+        };
+        _client = new DiscordSocketClient(config);
         _client.Log += Log;
 
         var token = File.ReadAllText("F:\\DiscordToken\\token.txt");
-
+        Console.WriteLine("1");
         await _client.LoginAsync(TokenType.Bot, token);
+        Console.WriteLine("2");
         await _client.StartAsync();
+        commands = new Commands(_client);
+        await commands.Client_Ready();
         await Task.Delay(-1);
 
+
     }
-    private static Task Log(LogMessage msg)
+    private Task Log(LogMessage msg)
     {
+        Console.WriteLine("3");
         Console.WriteLine(msg.ToString());
         return Task.CompletedTask;
     }
+
 
 
 }
